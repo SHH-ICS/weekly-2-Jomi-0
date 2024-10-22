5d53bc7d978830d1aab9ca6820dbd24e4a5468f0
Jomi O <78754002+Jomi-0@users.noreply.github.com>
2024-10-22 01:51:52 +0000

    fixing

--- handler.php
+++ handler.php
@@ -1,60 +1,21 @@
-<!DOCTYPE html>
-<html>
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-<head>
-  <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
-  <link rel="manifest" href="favicon/site.webmanifest">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-  <title>Welcome Page</title>
-  <!-- Uses a header that scrolls with the text, rather than staying
-  locked at the top -->
-  <div class="mdl-layout mdl-js-layout">
-    <header class="mdl-layout__header mdl-layout__header--scroll">
-      <div class="mdl-layout__header-row">
-        <!-- Title -->
-        <span class="mdl-layout-title">Title</span>
-        <!-- Add spacer, to align navigation to the right -->
-        <div class="mdl-layout-spacer"></div>
-        <!-- Navigation -->
-        <nav class="mdl-navigation">
-          <a class="mdl-navigation__link" href="https://legendary-capybara-6p5pgj5wj7w35pq9-8000.app.github.dev/">Home</a>
-          <a class="mdl-navigation__link" href="">Link</a>
-          <a class="mdl-navigation__link" href="">Link</a>
-          <a class="mdl-navigation__link" href="">Link</a>
-        </nav>
-      </div>
-    </header>
-    <div class="mdl-layout__drawer">
-      <span class="mdl-layout-title">Title</span>
-      <nav class="mdl-navigation">
-        <a class="mdl-navigation__link" href="">Link</a>
-        <a class="mdl-navigation__link" href="">Link</a>
-        <a class="mdl-navigation__link" href="">Link</a>
-        <a class="mdl-navigation__link" href="">Link</a>
-      </nav>
-    </div>
-    <main class="mdl-layout__content">
-      <div class="page-content"><!-- Your content goes here -->
-        <?php
-        $userName = "";
-        if (isset($_POST['userName'])) {
-          $userName = $_POST['userName'];
-        }
-        echo "<h1>Welcome " . $userName . "!</h1>\n";
-        ?>
-      </div>
-    </main>
-  </div>
-</head>
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['radius']) && is_numeric($_POST['radius'])) {
+  $radius = floatval($_POST['radius']);
+  $area = pi() * $radius * $radius;
+  $circumference = 2 * pi() * $radius;
 
-<body>
-
-
-
-</body>
-
-</html>
+  echo "<div style='text-align: center; margin-top: 50px;'>";
+  echo "<h1>Circle Results</h1>";
+  echo "<p>Radius: $radius units</p>";
+  echo "<p>Area: " . number_format($area, 2) . " square units</p>";
+  echo "<p>Circumference: " . number_format($circumference, 2) . " units</p>";
+  echo "</div>";
+} else {
+  echo "<div style='text-align: center; margin-top: 50px;'>";
+  echo "<h1>Invalid Input</h1>";
+  echo "<p>Please enter a valid numeric radius.</p>";
+  echo "</div>";
+}
