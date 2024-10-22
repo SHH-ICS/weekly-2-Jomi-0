868cc4cdff32615fd9bd878d02270cae2010a30f
Jomi O <78754002+Jomi-0@users.noreply.github.com>
2024-10-22 01:15:48 +0000

    commiting assingment 2

--- handler.php
+++ handler.php
@@ -1,23 +1,27 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <title>Welcome Page</title>
-  </head>
 
-  <body>
-    
-    <?php
-    $myVariable = "";
-    if ( isset( $_POST['myVariable'] ) ){
-      $myVariable = $_POST['myVariable'];
-    }
-    echo "<h1>My Program</h1>\n";
-    echo "<p>My Variable is = ".$myVariable."</p>\n";
-    ?>
-    
-  </body>
-  
+<head>
+  <title>Circle Calculator</title>
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+</head>
+
+<body>
+  <?php
+  if (isset($_POST['radius']) && is_numeric($_POST['radius'])) {
+    $radius = floatval($_POST['radius']);
+    $area = pi() * $radius * $radius;
+    $circumference = 2 * pi() * $radius;
+
+    echo "<h1>Circle Calculator</h1>";
+    echo "<p>Radius: $radius units</p>";
+    echo "<p>Area: " . number_format($area, 2) . " square units</p>";
+    echo "<p>Circumference: " . number_format($circumference, 2) . " units</p>";
+  } else {
+    echo "<h1>Invalid Input</h1>";
+    echo "<p>Please enter a valid numeric radius.</p>";
+  }
+  ?>
+</body>
+
 </html>
